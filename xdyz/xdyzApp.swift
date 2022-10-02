3a335c85bd639e1a1a7dccf68f45a7ba2d63fab4
marti844 <shikean@qq.com>
2022-10-02 19:28:31 +0800

    Initial Commit

---
+++ xdyz/xdyzApp.swift
@@ -0,0 +1,17 @@
+//
+//  xdyzApp.swift
+//  xdyz
+//
+//  Created by Kean Shi on 2022/10/2.
+//
+
+import SwiftUI
+
+@main
+struct xdyzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
